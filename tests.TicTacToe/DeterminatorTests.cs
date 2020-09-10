6389140b1aa827b88f14cd2c6580efe474d0a176
Linda Johnstone <linda.johnstone@myob.com>
2020-09-10 17:43:24 +1000

    Theory test DiagonalDeterminator

--- tests.TicTacToe/DeterminatorTests.cs
+++ tests.TicTacToe/DeterminatorTests.cs
@@ -1,5 +1,7 @@
 using Xunit;
 using TicTacToe;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace tests.TicTacToe
 {
@@ -9,14 +11,14 @@ namespace tests.TicTacToe
         public void ShouldTestWinningRow()
         {
             // arrange
-           int[][] board = new[] 
-            {                
-                new[] { 1, 1, 1 },                
-                new[] { 0, 0, 0 },                
-                new[] { 0, 0, 0 },                            
+            int[][] board = new[]
+             {
+                new[] { 1, 1, 1 },
+                new[] { 0, 0, 0 },
+                new[] { 0, 0, 0 },
             };
             var determinator = new RowDeterminator();
-            
+
             // act
             var result = determinator.IsThisAWin(board);
             // assert
@@ -27,13 +29,13 @@ namespace tests.TicTacToe
         public void ShouldTestWinningColumn()
         {
             // arrange
-            int[][] board = new[] 
-            {                
-                new[] { 1, 0, 0 },                
-                new[] { 1, 0, 0 },                
-                new[] { 1, 0, 0 },                            
+            int[][] board = new[]
+            {
+                new[] { 1, 0, 0 },
+                new[] { 1, 0, 0 },
+                new[] { 1, 0, 0 },
             };
-            var determinator = new ColumnDeterminator(); 
+            var determinator = new ColumnDeterminator();
 
             // act 
             var result = determinator.IsThisAWin(board);
@@ -45,11 +47,11 @@ namespace tests.TicTacToe
         public void ShouldTestWinningLeftRightDiagonal()
         {
             // arrange
-            int[][] board = new[] 
-            {                
-                new[] { 1, 0, 0 },                
-                new[] { 0, 1, 0 },                
-                new[] { 0, 0, 1 },                            
+            int[][] board = new[]
+            {
+                new[] { 1, 0, 0 },
+                new[] { 0, 1, 0 },
+                new[] { 0, 0, 1 },
             };
             var determinator = new DiagonalDeterminator();
 
@@ -59,15 +61,15 @@ namespace tests.TicTacToe
             // assert
             Assert.True(result);
         }
-        
+
         [Theory]
         [InlineData(new[] { 1, 0, 0 }, new[] { 0, 1, 0 }, new[] { 0, 0, 1 }, true)]
         public void ShouldTestWinningDiagonal(int[] one, int[] two, int[] three, bool expected)
         {
             // arrange
-            int[][] board = new[] 
-            {                
-                one, two, three                           
+            int[][] board = new[]
+            {
+                one, two, three
             };
             var determinator = new DiagonalDeterminator();
 
@@ -77,5 +79,72 @@ namespace tests.TicTacToe
             // assert
             Assert.Equal(expected, result);
         }
+        [Theory]
+        [ClassData(typeof(DeterminatorTestData))]
+        public void ShouldTestUsingClassData_WinningDiagonal(TestData data)
+        {
+            // arrange
+            var determinator = new DiagonalDeterminator();
+
+            // act
+            var result = determinator.IsThisAWin(data.Input);
+
+            // assert
+            Assert.Equal(data.Expected, result);
+        }
+        public class DeterminatorTestData : IEnumerable<object[]>
+        {
+            private readonly List<object[]> _testData = new List<object[]>
+            {
+                new object[]
+                {
+                    new TestData
+                    {
+                        Input = new[]
+                        {
+                            new[] {1, 0, 0},
+                            new[] {0, 1, 0},
+                            new[] {0, 0, 1},
+                        },
+                        Expected = true
+                    },
+                },
+                new object[]
+                {
+                    new TestData
+                    {
+                        Input = new[]
+                        {
+                            new[] {0, 0, 1},
+                            new[] {0, 1, 0},
+                            new[] {1, 0, 0},
+                        },
+                        Expected = true
+                    },
+                },
+                new object[]
+                {
+                    new TestData
+                    {
+                        Input = new[]
+                        {
+                            new[] {0, 0, 0},
+                            new[] {0, 0, 0},
+                            new[] {0, 0, 0},
+                        },
+                        Expected = false
+                    }
+                }
+            };
+
+            public IEnumerator<object[]> GetEnumerator() => _testData.GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+        public class TestData
+        {
+            public int[][] Input;
+            public bool Expected;
+        }
     }
 }
+
